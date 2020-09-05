b6a3dc9f6e017a7e16f25989f2184c28c4b9f736
zhanglulu <zhanglulu@cmhi.chinamobile.com>
2020-09-05 15:51:54 +0800

    add navigationView to the drawer

--- app/src/main/java/com/op/materialdesigndemo/ui/MainActivity.java
+++ app/src/main/java/com/op/materialdesigndemo/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.op.materialdesigndemo.ui;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -16,18 +17,20 @@ import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.op.materialdesigndemo.R;
 import com.op.materialdesigndemo.adapter.NewsAdapter;
 import com.op.materialdesigndemo.databinding.ActivityMainBinding;
+import com.op.materialdesigndemo.databinding.LayoutMenuHeaderBinding;
 import com.op.materialdesigndemo.entity.Story;
 import com.op.materialdesigndemo.vm.NewsViewModel;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener,
+        NavigationView.OnNavigationItemSelectedListener {
     private ActivityMainBinding binding;
     private NewsAdapter newsAdapter;
     private NewsViewModel viewModel;
@@ -39,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
+
+        binding.menu.navigation.setNavigationItemSelectedListener(this);
 
         binding.main.swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         binding.main.swipeRefresh.setOnRefreshListener(this);
@@ -66,7 +71,17 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 newsAdapter.setData(news);
             }
         });
+        viewModel.bitmapMutableLiveData.observe(this, new Observer<Bitmap>() {
+            @Override
+            public void onChanged(Bitmap bitmap) {
+                LayoutMenuHeaderBinding headerBinding = DataBindingUtil.bind(binding.menu.navigation.getHeaderView(0));
+                if (headerBinding != null) {
+                    headerBinding.avatar.setImageBitmap(bitmap);
+                }
+            }
+        });
         viewModel.getLatestNews();
+//        viewModel.getAvatar("");
     }
 
     @Override
@@ -92,6 +107,20 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     @Override
     public void onRefresh() {
         viewModel.getLatestNews();
+        viewModel.getAvatar("");
         binding.main.swipeRefresh.setRefreshing(false);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.footprint) {
+
+        } else if (item.getItemId() == R.id.settings) {
+
+        } else if (item.getItemId() == R.id.about) {
+
+        }
+        binding.drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
